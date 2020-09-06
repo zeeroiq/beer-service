23325f3d633148337118cfcac06d53a17cc90e44
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 01:50:05 +0530

    Project Initializes closes #1.

---
+++ src/test/java/com/shri/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shri.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
