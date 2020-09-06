45564946658631c501081728eac96eca937adb17
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 03:15:38 +0530

    Added Data model, closes #2

---
+++ src/main/java/com/shri/beerservice/controller/BeerController.java
@@ -0,0 +1,8 @@
+/*
+ * Created by  ZeeroIQ on 9/7/20, 2:20 AM
+ */
+
+package com.shri.beerservice.service.controller;
+
+public class BeerController {
+}
---
+++ src/test/java/com/shri/beerservice/controller/BeerControllerTest.java
@@ -0,0 +1,20 @@
+package com.shri.beerservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerControllerTest {
+
+    @Test
+    void getBeerById() {
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeerById() {
+    }
+}
