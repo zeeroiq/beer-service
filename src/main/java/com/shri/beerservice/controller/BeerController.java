8243a5938cef3c0029701b6e9bc0a4f3cf8376fc
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 03:21:56 +0530

    BeerController and relevant dependancy for h2, mockito[for testing] added. closes #3

--- src/main/java/com/shri/beerservice/controller/BeerController.java
+++ src/main/java/com/shri/beerservice/controller/BeerController.java
@@ -2,7 +2,34 @@
  * Created by  ZeeroIQ on 9/7/20, 2:20 AM
  */
 
-package com.shri.beerservice.service.controller;
+package com.shri.beerservice.controller;
 
+import com.shri.beerservice.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        // todo
+        return new ResponseEntity<>(BeerDto.builder().id(beerId).build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        // todo
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId) {
+        // todo
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
