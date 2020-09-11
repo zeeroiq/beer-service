8222fe6ba0405413db62f08cf4b3570791f2cf29
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-12 01:58:37 +0530

    Repository and services updated

--- src/main/java/com/shri/beerservice/repository/BeerRepository.java
+++ src/main/java/com/shri/beerservice/repository/BeerRepository.java
@@ -1,26 +1,14 @@
-/*
- * Created by  ZeeroIQ on 9/12/20, 1:21 AM
- */
-
 /*******************************************************************************
  * Created by  ZeeroIQ on 9/7/20, 2:12 AM
  ******************************************************************************/
 
 package com.shri.beerservice.repository;
 
+import com.shri.beerservice.domain.Beer;
 import com.shri.beerservice.model.BeerDto;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
+import java.util.UUID;
 
-public class BeerRepository extends PageImpl<BeerDto> {
-
-    public BeerRepository(List<BeerDto> content, Pageable pageable, long total) {
-        super(content, pageable, total);
-    }
-
-    public BeerRepository(List<BeerDto> content) {
-        super(content);
-    }
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
 }
--- src/main/java/com/shri/beerservice/service/BeerServiceImpl.java
+++ src/main/java/com/shri/beerservice/service/BeerServiceImpl.java
@@ -1,4 +1,43 @@
+/*
+ * Created by  ZeeroIQ on 9/12/20, 1:25 AM
+ */
+
 package com.shri.beerservice.service;
 
-public class BeerServiceImpl {
+import com.shri.beerservice.domain.Beer;
+import com.shri.beerservice.exceptions.NotFoundException;
+import com.shri.beerservice.mapper.BeerMapper;
+import com.shri.beerservice.model.BeerDto;
+import com.shri.beerservice.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID id) {
+        return beerMapper.beerToBeerDto(beerRepository.findById(id).orElseThrow(NotFoundException::new));
+    }
+
+    @Override
+    public BeerDto addBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUtc(beerDto.getUtc());
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
 }
