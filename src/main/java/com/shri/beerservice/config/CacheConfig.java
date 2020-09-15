e7d8552e6abaa5b8f0ba9901fc5689d415d8716b
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-16 01:55:33 +0530

    ehcache config related changes added. closes #27

---
+++ src/main/java/com/shri/beerservice/config/CacheConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Created by  ZeeroIQ on 9/16/20, 1:42 AM
+ */
+
+package com.shri.beerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
