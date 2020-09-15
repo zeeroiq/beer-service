3ced5bf900301808582213d3d02f795ec78abc18
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-15 23:54:35 +0530

    invalid commit. revert might be needed as tree is currupted.

--- src/main/java/com/shri/beerservice/controller/MyControllerException.java
+++
@@ -1,8 +0,0 @@
-/*
- * Created by  ZeeroIQ on 9/9/20, 2:25 AM
- */
-
-package com.shri.beerservice.controller;
-
-public class MyControllerException {
-}
--- src/main/java/com/shri/beerservice/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-/*
- * Created by  ZeeroIQ on 9/12/20, 1:42 AM
- */
-
-package com.shri.beerservice.exceptions;
-
-public class NotFoundException extends RuntimeException{
-}
